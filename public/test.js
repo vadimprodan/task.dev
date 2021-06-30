764a4b0d5d0d8716e049c42e7480887bedc0602f
Vadim Prodan <vprodan@yukon.cv.ua>
2021-06-30 16:17:41 +0300

    Create test.js

---
+++ public/test.js
@@ -0,0 +1,18 @@
+var FBAppScript = document.createElement('script');
+FBAppScript.setAttribute('src', 'https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js');
+document.body.append(FBAppScript);
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBe6ooH-1rrcd8ui6ec7TtARe42Lh4iSO8",
+  authDomain: "edge-srv.firebaseapp.com",
+  projectId: "edge-srv",
+  storageBucket: "edge-srv.appspot.com",
+  messagingSenderId: "576813547971",
+  appId: "1:576813547971:web:5e9aced002ed4fadd07d76",
+  measurementId: "G-G8JKZYGM57"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
