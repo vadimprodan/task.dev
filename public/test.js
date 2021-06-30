98646bb3b8e5b313c4a36f999708c6c3934208f9
Vadim Prodan <vprodan@yukon.cv.ua>
2021-06-30 16:36:23 +0300

    Update test.js

--- public/test.js
+++ public/test.js
@@ -1,20 +1,17 @@
-var FBAppScript = document.createElement('script');
-FBAppScript.setAttribute('src', 'https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js?callback=init');
-document.body.append(FBAppScript);
-
-var firebaseConfig = {
-  apiKey: "AIzaSyBe6ooH-1rrcd8ui6ec7TtARe42Lh4iSO8",
-  authDomain: "edge-srv.firebaseapp.com",
-  projectId: "edge-srv",
-  storageBucket: "edge-srv.appspot.com",
-  messagingSenderId: "576813547971",
-  appId: "1:576813547971:web:5e9aced002ed4fadd07d76",
-  measurementId: "G-G8JKZYGM57"
-};
-
-// Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-
-function init() {
-  console.log('test');
-}
+fetch('https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js').then(() => {
+  var FBAppScript = document.createElement('script');
+  FBAppScript.setAttribute('src', 'https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js');
+  document.body.append(FBAppScript);
+  
+  var firebaseConfig = {
+    apiKey: "AIzaSyBe6ooH-1rrcd8ui6ec7TtARe42Lh4iSO8",
+    authDomain: "edge-srv.firebaseapp.com",
+    projectId: "edge-srv",
+    storageBucket: "edge-srv.appspot.com",
+    messagingSenderId: "576813547971",
+    appId: "1:576813547971:web:5e9aced002ed4fadd07d76",
+    measurementId: "G-G8JKZYGM57"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+});
